405b79bdb3de6cb6157037399e7de774cb427e89
Vincent Taylor <vinny.taylor17@gmail.com>
2024-09-14 18:59:27 -0400

    starting to work on a menu

--- blankblankofblank_current.py
+++ blankblankofblank_current.py
@@ -9,6 +9,7 @@ battle = False
 out = False
 money = 100000000
 nav = 0
+currentPosition = 0
 
 # pygame setup
 pygame.init()
@@ -26,12 +27,11 @@ BLACK = (0, 0, 0)
 GREEN = (0, 255, 0)
 
 # declaring the list that will contain the visible lines of text
-numLines = 23
+numLines = 24
 lineWidth = 80
 visibleText = []
 for i in range(numLines):
     visibleText.append("".ljust(lineWidth))
-currentUserInput = "".ljust(lineWidth)
 
 # function to print a line to the terminal window
 def printToTerminal(newLine):
@@ -41,15 +41,11 @@ def printToTerminal(newLine):
 
 def clearTerminal():
     os.system('cls')
-    print("clearing terminal")
     for i in range(numLines):
         visibleText.pop(0)
         visibleText.append("".ljust(lineWidth))
 
-def getUserInput():
-    command = currentUserInput
-    currentUserInput = ""
-    return command
+
 
 
 
@@ -106,21 +102,31 @@ def uiTop():
     printToTerminal("---------------------------------------------")
 def uiBot(thisText = "> "):
     printToTerminal("---------------------------------------------")
-    printToTerminal("{}".format(thisText))
-def menu1():
+    #printToTerminal("{}".format(thisText))
+def menu1(currentPosition):
     printToTerminal("please make a selection")
-    printToTerminal("1. start")
-    printToTerminal("2. battle")
-    printToTerminal("3. quit")
+    if(currentPosition == 0):
+        printToTerminal("[1] start")
+        printToTerminal(" 2. battle")
+        printToTerminal(" 3. quit")
+    elif (currentPosition == 1):
+        printToTerminal(" 1. start")
+        printToTerminal("[2] battle")
+        printToTerminal(" 3. quit")
+    elif (currentPosition == 2):
+        printToTerminal(" 1. start")
+        printToTerminal(" 2. battle")
+        printToTerminal("[3] quit")
     
-    
+
 
 #main playing loop
 while playing:
 
     # poll for events
+    events = pygame.event.get()
     # pygame.QUIT event means the user clicked X to close your window
-    for event in pygame.event.get():
+    for event in events:
         if event.type == pygame.QUIT:
             playing = False
 
@@ -129,131 +135,140 @@ while playing:
     clearTerminal()
 
     uiTop()
-    menu1()
+    menu1(currentPosition)
     uiBot()
     
     # if we're in the main menu
-    # if mainMenu: 
-    #     clearTerminal()
-    #     nav=0
+    if mainMenu: 
+        clearTerminal()
+        nav=0
         
-    #     uiTop() #print the UI
-    #     menu1()
-    #     uiBot()
+        uiTop() #print the UI
+        menu1(currentPosition)
+        uiBot()
 
-    #     nav = int(input()) # get input from user to navigate
+        for event in events:
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_UP:
+                    currentPosition = (currentPosition - 1) % 3
+                elif event.key == pygame.K_DOWN:
+                    currentPosition = (currentPosition + 1) % 3
+                if event.key == pygame.K_RETURN:
+                    nav = currentPosition + 1
+
+        #nav = int(input()) # get input from user to navigate
         
 
-    #     # if it's 1, 2, or 3, we move to a different screen
-    #     if nav == 1:
-    #         mainMenu = False
-    #         shop = True
-    #     elif nav == 2:
-    #         mainMenu = False
-    #         battle = True
-    #     elif nav == 3:
-    #         out = True
-    #         mainMenu = False
+        # if it's 1, 2, or 3, we move to a different screen
+        if nav == 1:
+            mainMenu = False
+            shop = True
+        elif nav == 2:
+            mainMenu = False
+            battle = True
+        elif nav == 3:
+            out = True
+            mainMenu = False
 
-    # if shop:
-    #     clearTerminal()
-    #     nav = 0
-    #     infoText = "Please make a selection to re-roll that aspect!\n5 to go back to the main menu\n6 to save current item to hall of fame"
-    #     uiTop()
-    #     shopDisp(myItem.getList())
-    #     uiBot(infoText)
-    #     nav = int(input())
-    #     if (nav == 1 or nav == 2 or nav == 3) and (money >= 3): #money-3 == 0?
-    #         money -= 3
-    #         clearTerminal()
-    #         myItem.roll(nav)
+    if shop:
+        clearTerminal()
+        nav = 0
+        infoText = "Please make a selection to re-roll that aspect!\n5 to go back to the main menu\n6 to save current item to hall of fame"
+        uiTop()
+        shopDisp(myItem.getList())
+        uiBot(infoText)
+        #nav = int(input())
+        if (nav == 1 or nav == 2 or nav == 3) and (money >= 3): #money-3 == 0?
+            money -= 3
+            clearTerminal()
+            myItem.roll(nav)
 
-    #     elif (nav == 4) and (money >= 1):
-    #         money = money - 1
-    #         clearTerminal()
-    #         myItem.roll(nav)
+        elif (nav == 4) and (money >= 1):
+            money = money - 1
+            clearTerminal()
+            myItem.roll(nav)
 
-    #     elif (nav == 1 or nav == 2 or nav == 3 or nav == 4) and (money - 3 <= 0):
-    #         clearTerminal()
-    #         infoText = "not enough money!\n enter to continue"
-    #         input()
+        elif (nav == 1 or nav == 2 or nav == 3 or nav == 4) and (money - 3 <= 0):
+            clearTerminal()
+            infoText = "not enough money!\n enter to continue"
+            input()
 
-    #     elif nav == 5:
-    #         clearTerminal()
-    #         infoText = "1. main menu\n2. battle screen"
-    #         uiTop()
-    #         shopDisp(myItem.getList())
-    #         uiBot(infoText)
-    #         nav = int(input())
-    #         if nav == 1:
-    #             mainMenu = True
-    #             shop = False
-    #         elif nav == 2:
-    #             shop = False
-    #             battle = True
-    #     elif nav == 6:
-    #         clearTerminal()
-    #         with open("user saved.txt", "a+") as file:
-    #             file.write("\n" + myItem.toReadable())
-    #         #savedItems.write("{}".format(myItem.toReadable()))
-    #         infoText = "Saved!"
-    #         uiTop()
-    #         shopDisp(myItem.getList())
-    #         uiBot(infoText)
-    #         input()
+        elif nav == 5:
+            clearTerminal()
+            infoText = "1. main menu\n2. battle screen"
+            uiTop()
+            shopDisp(myItem.getList())
+            uiBot(infoText)
+            #nav = int(input())
+            if nav == 1:
+                mainMenu = True
+                shop = False
+            elif nav == 2:
+                shop = False
+                battle = True
+        elif nav == 6:
+            clearTerminal()
+            with open("user saved.txt", "a+") as file:
+                file.write("\n" + myItem.toReadable())
+            #savedItems.write("{}".format(myItem.toReadable()))
+            infoText = "Saved!"
+            uiTop()
+            shopDisp(myItem.getList())
+            uiBot(infoText)
+            input()
 
-    # if battle:
-    #     i = 1
-    #     combo = 0
-    #     newenemy = Enemy(i)
+    if battle:
+        i = 1
+        combo = 0
+        newenemy = Enemy(i)
 
-    #     nav = int(input())
-    #     if nav == 1:
-    #         attack = True
-    #         printToTerminal('{} attacks!!!!!! he has {} health and {} defence!\nenter to attack!'.format(newenemy.name, newenemy.hp, newenemy.defence))
-    #     elif nav == 2:
-    #         battle = False
-    #         shop = True
-    #     elif attack:
-    #         if newenemy.hp - pc.itemDamage <= 0:
-    #             clearTerminal()
-    #             printToTerminal("enemies beaten: {}\nitem attack: {}\ncurrent health: {}".format(combo, pc.itemDamage, pc.playerHp)) 
-    #             printToTerminal("you won!")
-    #             input()
-    #             newenemy = Enemy(i)
-    #             printToTerminal('uh oh!\n{} attacks!!!!!! he has {} health and {} defence!\nenter to attack!'.format(newenemy.name, newenemy.hp, newenemy.defence))
-    #             combo +=1
-    #             i += 1
-    #             pc.itemDamage += 1
-    #             input()
+        #nav = int(input())
+        if nav == 1:
+            attack = True
+            printToTerminal('{} attacks!!!!!! he has {} health and {} defence!\nenter to attack!'.format(newenemy.name, newenemy.hp, newenemy.defence))
+        elif nav == 2:
+            battle = False
+            shop = True
+        elif attack:
+            if newenemy.hp - pc.itemDamage <= 0:
+                clearTerminal()
+                printToTerminal("enemies beaten: {}\nitem attack: {}\ncurrent health: {}".format(combo, pc.itemDamage, pc.playerHp)) 
+                printToTerminal("you won!")
+                input()
+                newenemy = Enemy(i)
+                printToTerminal('uh oh!\n{} attacks!!!!!! he has {} health and {} defence!\nenter to attack!'.format(newenemy.name, newenemy.hp, newenemy.defence))
+                combo +=1
+                i += 1
+                pc.itemDamage += 1
+                input()
             
-    #         elif pc.playerHp - newenemy.att <= 0:
-    #             clearTerminal()
-    #             printToTerminal('you died!')
-    #             input()
-    #             battle = False
-    #             Shop = True
+            elif pc.playerHp - newenemy.att <= 0:
+                clearTerminal()
+                printToTerminal('you died!')
+                input()
+                battle = False
+                Shop = True
 
             
-    #         else:
-    #             clearTerminal()
-    #             printToTerminal("enemies beaten: {}\nitem attack: {}\ncurrent health: {}".format(combo, pc.itemDamage, pc.playerHp))
-    #             newenemy.damage(pc.itemDamage)
-    #             pc.damagePlayer(newenemy.att)
-    #             printToTerminal("{}'s current health is {} with defence {}".format(newenemy.name, newenemy.hp, newenemy.defence))
-    #             input()
+            else:
+                clearTerminal()
+                printToTerminal("enemies beaten: {}\nitem attack: {}\ncurrent health: {}".format(combo, pc.itemDamage, pc.playerHp))
+                newenemy.damage(pc.itemDamage)
+                pc.damagePlayer(newenemy.att)
+                printToTerminal("{}'s current health is {} with defence {}".format(newenemy.name, newenemy.hp, newenemy.defence))
+                input()
             
-    # if out:
-    #     nav = 0
-    #     uiTop()
-    #     printToTerminal('are you sure?')
-    #     uiBot()
-    #     nav = int(input())
-    #     if nav == 1:
-    #         exit()
-    #     elif nav == 0:
-    #         mainMenu = True
-    #         out = False
+    if out:
+        nav = 0
+        uiTop()
+        printToTerminal('are you sure?')
+        uiBot()
+        #nav = int(input())
+        if nav == 1:
+            exit()
+        elif nav == 0:
+            mainMenu = True
+            out = False
 
 
    
@@ -267,8 +282,6 @@ while playing:
         #pygame.draw.rect(img, RED, rect, 1)
         screen.blit(img, (5, height))
         height = height + fontSize + 1
-    img = font.render(currentUserInput, True, GREEN)
-    screen.blit(img, (5, height))
 
     # flip() the display to put your work on screen
     pygame.display.flip()
